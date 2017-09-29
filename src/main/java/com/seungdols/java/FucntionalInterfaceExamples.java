d221f03bb7ca23f9543327a8beeb0c693a57a27a
seungdols <seungdols0822@gmail.com>
2017-09-29 18:17:51 +0900

    실전 예제 - lombok 사용후

--- src/main/java/com/seungdols/java/FucntionalInterfaceExamples.java
+++ src/main/java/com/seungdols/java/FucntionalInterfaceExamples.java
@@ -1,6 +1,15 @@
 package com.seungdols.java;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * @PACKAGE com.seungdols.java
@@ -10,79 +19,57 @@ import java.math.BigDecimal;
 public class FucntionalInterfaceExamples {
 
 	public static void main(String[] args) {
+		final List<Product> products = Arrays.asList(
+			new Product(1L, "A", new BigDecimal("10.100")),
+			new Product(2L, "B", new BigDecimal("60.100")),
+			new Product(3L, "C", new BigDecimal("80.160")),
+			new Product(4L, "D", new BigDecimal("40.100")),
+			new Product(5L, "E", new BigDecimal("30.50")),
+			new Product(6L, "F", new BigDecimal("20.10"))
+		);
 
+		List<Product> result = filter(products, product -> product.getPrice().compareTo(new BigDecimal("20")) >= 0);
+		System.out.println(result);
+
+		final List<Product> expensiveProducts = filter(products, product -> product.getPrice().compareTo(new BigDecimal("50")) > 0);
+
+		final List<DiscoutedProduct> discoutedProducts = map(expensiveProducts, product -> new DiscoutedProduct(product.getId(), product.getName(), product.getPrice().multiply(new BigDecimal("0.5"))));
+
+		System.out.println("expensiveProducts : " + expensiveProducts);
+		System.out.println("discountedProducts : " + discoutedProducts);
+	}
+
+	private static <T> List<T> filter(List<T> list, Predicate<T> predicate) {
+		List<T> result = new ArrayList<>();
+		for (final T t : list) {
+			if (predicate.test(t)) {
+				result.add(t);
+			}
+		}
+		return result;
+	}
+
+	private static <T, R> List<R> map(List<T> list, Function<T, R> function) {
+		List<R> result = new ArrayList<>();
+		for (final T t : list) {
+			result.add(function.apply(t));
+		}
+		return result;
 	}
 
 }
 
-class Proudct {
+@AllArgsConstructor
+@Data
+class Product {
 	private Long id;
 	private String name;
 	private BigDecimal price;
+}
 
-	public Proudct() {
-	}
-
-	public Proudct(Long id, String name, BigDecimal price) {
-		this.id = id;
-		this.name = name;
-		this.price = price;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public BigDecimal getPrice() {
-		return price;
-	}
-
-	public void setPrice(BigDecimal price) {
-		this.price = price;
-	}
-
-	@Override
-	public String toString() {
-		return "Proudct{" +
-			"id=" + id +
-			", name='" + name + '\'' +
-			", price=" + price +
-			'}';
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-
-		Proudct proudct = (Proudct)o;
-
-		if (id != null ? !id.equals(proudct.id) : proudct.id != null)
-			return false;
-		if (name != null ? !name.equals(proudct.name) : proudct.name != null)
-			return false;
-		return price != null ? price.equals(proudct.price) : proudct.price == null;
-	}
-
-	@Override
-	public int hashCode() {
-		int result = id != null ? id.hashCode() : 0;
-		result = 31 * result + (name != null ? name.hashCode() : 0);
-		result = 31 * result + (price != null ? price.hashCode() : 0);
-		return result;
+@ToString(callSuper = true)
+class DiscoutedProduct extends Product {
+	public DiscoutedProduct(Long id, String name, BigDecimal price) {
+		super(id, name, price);
 	}
 }
